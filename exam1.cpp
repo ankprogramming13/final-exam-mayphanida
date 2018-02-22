08035f886c15dbfa56ae957a1449aaf9984d9fd8
Phanida Thaijamnong <phanidatha@cvc-cha.ac.th>
2018-02-22 12:10:48 +0700

    ข้อ 1 ข้อ 2

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+    int a, b;
+    char e;
+    int cmd;
+
+    cout << "********Welcome to CLI Calculator V1.00********" << endl;
+    cout << "Enter Number1 : ";
+    cin >> a;
+    cout << "Enter Number2 : ";
+    cin >> b;
+
+    cin >> cmd;
+    if (cmd == 1){
+        cout << endl;
+        cout << "Sum" << a << "+" << b;
+        cout << " = " << a + b << endl;
+        cout << endl;
+    } else if (cmd == 2) {
+        cout << "Sum" << a << "-" << b;
+        cout << " = " << a - b << endl;
+        cout << endl;
+    } else if (cmd == 3) {
+        cout << "Sum" << a << "*" << b;
+        cout << " = " << a * b << endl;
+        cout << endl;
+    } else if (cmd == 4) {
+        cout << "Sum" << a << "/" << b;
+        cout << " = " << a / b << endl;
+        cout << endl;
+    }
+
+        while(e!='q');
+        cout << "\n###END Process###\t" << endl;
 
         return 0;
 }
--- exam2.cpp
+++ exam2.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
 int main() {
+    int cmd;
+        ofstream writefile ("std.txt", ofstream::app);
+        ofstream myfile ("std.txt");
+        ifstream readfile ("std.txt");
+        string line;
+        string name;
+
+        cin >> cmd;
+        if (cmd== 1) {
+            cin >> name;
+            if (writefile.is_open()) {
+                writefile << name << endl;
+                writefile.close ();
+            } else {
+                cout << "Unable to open file";
+                }
+            } else if (cmd == 2) {
+                if (readfile.is_open()) {
+                    while (getline(readfile,line)) {
+                        cout << line << '\n';
+                    }
+                    readfile.close();
+                }
+
+            } else if (cmd == 3) {
+
+            } else if (cmd == 5) {
+                 if (myfile.is_open()) {}
+
+            }
 
         return 0;
 }
